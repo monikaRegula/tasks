d9fd3cbfb01468b0631d4c0c59fc0c863bfb39d1
Monika <monikaregula97@gmail.com>
2019-04-27 17:46:05 +0200

    deploy na zewnętrznym serwerze Tomcat

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == 0 goto startPage
+echo.
+echo Cannot start runcrud.bat
+
+:startPage
+cd C:\Users\asus\Program Files\Google\Chrome\Application
+start chrome.exe http://localhost:8181/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == 0 goto end
+echo.
+echo Cannot start Google Chrome
+
+:fail
+echo.
+echo There were some errors;
+
+:end
+echo.
+echo Work is finished.
