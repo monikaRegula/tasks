770ded7db7656d8d8cbdb073ae50808c6fbad19b
Monika <monikaregula97@gmail.com>
2019-05-08 14:51:50 +0200

    ff

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableSwagger2//włącza obsługę Swaggera
 @Configuration
@@ -22,7 +22,7 @@ public class CoreConfiguration implements WebMvcConfigurer {
         return new RestTemplate();
     }
 
-    @Bean
+    /*@Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)//wybiera typ dokumentacji
                 .select()//rozpoczęcie budowania konfiguracji dla typu dokumentacji SWAGGER_2
@@ -41,5 +41,5 @@ public class CoreConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(0);
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+    }*/
 }
